31911193320ebea29dae60d13bc1899417a47ad8
namet117 <namet117@163.com>
2019-02-11 11:41:02 +0800

    update ubuntu-init

--- server-init/ubuntu.sh
+++ server-init/ubuntu.sh
@@ -7,5 +7,6 @@ apt upgrade -y
 apt install -y vim curl
 
 # VIM config
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 curl -o /root/namet.vim -L https://raw.githubusercontent.com/namet117/vimrc/master/namet.vim
 echo "source /root/namet.vim" > /root/.vimrc
