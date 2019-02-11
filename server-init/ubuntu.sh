ac610d676389f1b4531df22eb99e38565e427935
namet117 <namet117@163.com>
2019-02-11 15:31:20 +0800

    update ubuntu-init

--- server-init/ubuntu.sh
+++ server-init/ubuntu.sh
@@ -4,9 +4,27 @@ apt update
 
 apt upgrade -y
 
-apt install -y vim curl
+apt install -y git vim curl
+
+
+# Install Nodejs
+curl -o /tmp/node.tar.xz -L https://nodejs.org/dist/v11.9.0/node-v11.9.0-linux-x64.tar.xz
+
+xz -d /tmp/node.tar.xz
+
+tar -xvf /tmp/node.tar
+
+mv /tmp/node /opt/node
+
+ln -s /opt/node/bin/node /usr/bin/node
+ln -s /opt/node/bin/npm /usr/bin/npm
+ln -s /opt/node/bin/npx /usr/bin/npx
 
 # VIM config
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-curl -o /root/namet.vim -L https://raw.githubusercontent.com/namet117/vimrc/master/namet.vim
-echo "source /root/namet.vim" > /root/.vimrc
+
+curl -o ~/namet.vim -L https://raw.githubusercontent.com/namet117/vimrc/master/namet.vim
+
+echo "source ~/namet.vim" > ~/.vimrc
+
+vim +PlugInstall
