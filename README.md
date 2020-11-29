f6cfa1b9e361e6a786bd832ca495c78606574e4e
namet117 <namet117@163.com>
2020-11-29 17:37:10 +0800

    更新ubuntu脚本

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 常用工具
 
-* [在ubuntu(18.04/16.04)上安装php开发环境](https://github.com/namet117/tools/tree/master/ubuntu-install-php)
+* [在ubuntu上初始化开发环境](https://github.com/namet117/tools/tree/master/server-init)
--- server-init/ubuntu.sh
+++ server-init/ubuntu.sh
@@ -1,10 +1,42 @@
 #!/bin/bash
 
-apt update
 
-apt upgrade -y
+# 定义函数
+showError() {
+    echo -e "\e[1;31m [ Error ] $1\e[0m";
+    exit 1;
+}
 
-apt install -y git vim curl
+showSuccess() {
+    echo -e "\e[1;32m$1\e[0m";
+}
+
+showInfo() {
+    echo -e "$1";
+}
+
+
+# 0. 判断是否为Ubuntu
+if [[ $(cat /etc/os-release | grep "NAME=\"Ubuntu\"") == '' ]]; then
+
+if
+
+# 1. 更换源为国内的地址
+if [[ $(cat /etc/apt/sources.list | grep "ubuntu.com") != '' ]]; then
+    sudo cp /etc/apt/sources.list /etc/apt/sources.list.original;
+    sudo sed -i "s@http://.*archive.ubuntu.com@http://repo.huaweicloud.com@g" /etc/apt/sources.list;
+    sudo sed -i "s@http://.*security.ubuntu.com@http://repo.huaweicloud.com@g" /etc/apt/sources.list;
+fi
+
+# 2. 更新
+sudo apt update
+
+
+sudo apt install -y git vim unzip \
+        nginx mysql-server mysql-client redis-server \
+        php php-cli php-fpm php-dev libnghttp2-dev \
+        php-bcmath php-bz2 php-curl php-gd php-json php-imap php-mbstring \
+        php-odbc php-soap php-pgsql php-sqlite3 php-xml php-tidy php-zip;
 
 
 # Install Nodejs
